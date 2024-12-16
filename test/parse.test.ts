c5c6ee111b405b91a16aa05a325397e8722e2d33
Bernhard Pottler <bpottler@nexgo.de>
2024-12-16 14:21:32 +0100

    refactor(test): rename 'parser.test.ts' to 'parse.test.ts'

--- test/parse.test.ts
+++ test/parse.test.ts
@@ -18,5 +18,7 @@ it('format', () => {
   expect(inst.format('YYYY-MM-DD HH:mm')).toBe('2021-01-01 00:00')
   expect(inst.format('YYYY-MM-DD HH:mm:ss')).toBe('2021-01-01 00:00:00')
   expect(inst.format('YYYY-MM-DD HH:mm:ss.SSS')).toBe('2021-01-01 00:00:00.000')
+  // format: 'YYYY year MM month DD day HH hour mm minute ss second' in simplified chinese
+  // result: 'January 01, 2021 00 hour 00 minute 00 second' in simplified chinese
   expect(inst.format('YYYY年MM月DD日 HH时mm分ss秒')).toBe('2021年01月01日 00时00分00秒')
 })
