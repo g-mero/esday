30da86ba612e02fb62fce5dc4ba09ebdf2c863ee
g-mero <gmero@foxmail.com>
2024-12-23 11:35:24 +0800

    test: add plugin locale test

---
+++ test/plugins/locale.test.ts
@@ -0,0 +1,53 @@
+/* eslint-disable dot-notation */
+import { esday } from 'esday'
+import { describe, expect, it } from 'vitest'
+import localeZh from '~/locales/zh'
+import { localePlugin } from '~/plugins/locale'
+
+esday.extend(localePlugin)
+
+describe('factory locale methods', () => {
+  it('set locale', () => {
+    esday.locale('zh')
+    // @ts-expect-error $l is private property
+    expect(esday()['$l']).toBe('zh')
+
+    esday.locale('non-exist')
+    // @ts-expect-error $l is private property
+    expect(esday()['$l']).toBe('non-exist')
+  })
+
+  it('register locale', () => {
+    esday.registerLocale(localeZh)
+    esday.locale('zh')
+    // @ts-expect-error $locale is private method
+    expect(esday().$locale()).toBe(localeZh)
+
+    esday.registerLocale(localeZh, 'zh-cn')
+    esday.locale('zh-cn')
+    // @ts-expect-error $locale is private method
+    expect(esday().$locale()).toBe(localeZh)
+  })
+})
+
+describe('esDay locale methods', () => {
+  const day = esday('2021-01-01')
+  it('set locale', () => {
+    // @ts-expect-error $l is private property
+    expect(day['$l']).toBe('en')
+    const dayZh = day.locale('zh')
+    // @ts-expect-error $l is private property
+    expect(dayZh['$l']).toBe('zh')
+  })
+
+  it('start of', () => {
+    esday.registerLocale({
+      name: 'test',
+      weekStart: 0,
+    } as any)
+    const dayTest = day.locale('test')
+    // @ts-expect-error $locale is private method
+    expect(dayTest.$locale().weekStart).toBe(0)
+    expect(dayTest.startOf('week').format('YYYY-MM-DD')).toBe('2020-12-27')
+  })
+})
