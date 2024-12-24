dda7c1a38a7f66bfd6dfb8fc15d7670ba8767282
Bernhard Pottler <bpottler@nexgo.de>
2024-12-24 08:14:34 +0100

    feat: add locale for en-us by using 'en' as a base
    This is also a template for Locales of a territory based on
    the Locale of a language

---
+++ src/locales/en-us.ts
@@ -0,0 +1,27 @@
+/**
+ * English (United States) [en-US]
+ * This locale shows how to create a locale for a territory based
+ * on the locale of the corresponding language.
+ */
+
+import localeEn from '~/locales/en'
+import { cloneLocale, setLocaleProperty } from '~/plugins/locale'
+
+const localeEnUs = cloneLocale(localeEn)
+const localeEnUsFormats = {
+  LT: 'h:mm a',
+  LTS: 'h:mm:ss a',
+  L: 'MM/DD/YYYY',
+  LL: 'MMMM D, YYYY',
+  LLL: 'MMMM D, YYYY h:mm a',
+  LLLL: 'dddd, MMMM D, YYYY h:mm a',
+
+}
+
+// Use 'setLocaleProperty' as all properties are 'readonly'
+setLocaleProperty(localeEnUs, 'name', 'en-US')
+setLocaleProperty(localeEnUs, 'weekStart', 0)
+setLocaleProperty(localeEnUs, 'yearStart', 1)
+setLocaleProperty(localeEnUs, 'key', localeEnUsFormats)
+
+export default localeEnUs
