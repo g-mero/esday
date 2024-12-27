2fe3d9701989b04eb2dda4ba200e3881ebf0d0d7
Bernhard Pottler <bpottler@nexgo.de>
2024-12-27 20:00:49 +0100

    feat: add locale for 'rn'

---
+++ src/locales/rn.ts
@@ -0,0 +1,80 @@
+/**
+ * Kirundi [rn]
+ */
+
+import type { Locale } from '~/types'
+
+const localeRn: Readonly<Locale> = {
+  name: 'rn',
+  weekdays: [
+    'Ku wa Mungu',
+    'Ku wa Mbere',
+    'Ku wa Kabiri',
+    'Ku wa Gatatu',
+    'Ku wa Kane',
+    'Ku wa Gatanu',
+    'Ku wa Gatandatu',
+  ],
+  weekdaysShort: ['Kngu', 'Kmbr', 'Kbri', 'Ktat', 'Kkan', 'Ktan', 'Kdat'],
+  weekdaysMin: ['K7', 'K1', 'K2', 'K3', 'K4', 'K5', 'K6'],
+  months: [
+    'Nzero',
+    'Ruhuhuma',
+    'Ntwarante',
+    'Ndamukiza',
+    'Rusama',
+    'Ruhenshi',
+    'Mukakaro',
+    'Myandagaro',
+    'Nyakanga',
+    'Gitugutu',
+    'Munyonyo',
+    'Kigarama',
+  ],
+  monthsShort: [
+    'Nzer',
+    'Ruhuh',
+    'Ntwar',
+    'Ndam',
+    'Rus',
+    'Ruhen',
+    'Muk',
+    'Myand',
+    'Nyak',
+    'Git',
+    'Muny',
+    'Kig',
+  ],
+  weekStart: 1,
+  yearStart: 4,
+  formats: {
+    LT: 'HH:mm',
+    LTS: 'HH:mm:ss',
+    L: 'DD/MM/YYYY',
+    LL: 'D MMMM YYYY',
+    LLL: 'D MMMM YYYY HH:mm',
+    LLLL: 'dddd, D MMMM YYYY HH:mm',
+  },
+  relativeTime: {
+    future: 'mu %s',
+    past: '%s',
+    s: 'amasegonda',
+    m: 'Umunota',
+    mm: '%d iminota',
+    h: 'isaha',
+    hh: '%d amasaha',
+    d: 'Umunsi',
+    dd: '%d iminsi',
+    M: 'ukwezi',
+    MM: '%d amezi',
+    y: 'umwaka',
+    yy: '%d imyaka',
+  },
+  meridiem: (hour: number, minute: number, isLowercase: boolean) => {
+    const m = (hour < 12 ? 'AM' : 'PM')
+    return isLowercase ? m.toLowerCase() : m
+  },
+  ordinal: n => `${n}.`,
+}
+
+export default localeRn
