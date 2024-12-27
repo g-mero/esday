bf66604a514863dafeb9f1872c21320be601ed0b
Bernhard Pottler <bpottler@nexgo.de>
2024-12-27 20:01:19 +0100

    feat: add locale for 'de'

---
+++ src/locales/de.ts
@@ -0,0 +1,86 @@
+/**
+ * German [de]
+ */
+
+import type { Locale } from '~/types'
+
+const relativeTimeFormatStrings = {
+  s: 'ein paar Sekunden',
+  m: ['eine Minute', 'einer Minute'],
+  mm: '%d Minuten',
+  h: ['eine Stunde', 'einer Stunde'],
+  hh: '%d Stunden',
+  d: ['ein Tag', 'einem Tag'],
+  dd: ['%d Tage', '%d Tagen'],
+  M: ['ein Monat', 'einem Monat'],
+  MM: ['%d Monate', '%d Monaten'],
+  y: ['ein Jahr', 'einem Jahr'],
+  yy: ['%d Jahre', '%d Jahren'],
+}
+
+function relativeTimeFormatter(timeValue: string | number, withoutSuffix: boolean, range: string) {
+  let result = ''
+  const l = relativeTimeFormatStrings[range as keyof typeof relativeTimeFormatStrings]
+  if (Array.isArray(l)) {
+    result = l[withoutSuffix ? 0 : 1]
+  }
+  else {
+    result = l
+  }
+
+  return result.replace('%d', timeValue.toString())
+}
+
+const localeDe: Readonly<Locale> = {
+  name: 'de',
+  weekdays: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+  weekdaysShort: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+  weekdaysMin: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+  months: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+  monthsShort: [
+    'Jan.',
+    'Feb.',
+    'März',
+    'Apr.',
+    'Mai',
+    'Juni',
+    'Juli',
+    'Aug.',
+    'Sept.',
+    'Okt.',
+    'Nov.',
+    'Dez.',
+  ],
+  weekStart: 1,
+  yearStart: 4,
+  formats: {
+    LT: 'HH:mm',
+    LTS: 'HH:mm:ss',
+    L: 'DD.MM.YYYY',
+    LL: 'D. MMMM YYYY',
+    LLL: 'D. MMMM YYYY HH:mm',
+    LLLL: 'dddd, D. MMMM YYYY HH:mm',
+  },
+  relativeTime: {
+    future: 'in %s',
+    past: 'vor %s',
+    s: relativeTimeFormatter,
+    m: relativeTimeFormatter,
+    mm: relativeTimeFormatter,
+    h: relativeTimeFormatter,
+    hh: relativeTimeFormatter,
+    d: relativeTimeFormatter,
+    dd: relativeTimeFormatter,
+    M: relativeTimeFormatter,
+    MM: relativeTimeFormatter,
+    y: relativeTimeFormatter,
+    yy: relativeTimeFormatter,
+  },
+  meridiem: (hour: number, minute: number, isLowercase: boolean) => {
+    const m = (hour < 12 ? 'AM' : 'PM')
+    return isLowercase ? m.toLowerCase() : m
+  },
+  ordinal: n => `${n}.`,
+}
+
+export default localeDe
