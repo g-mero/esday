61367309b3014a9363353617f0bd13e1dc7be24f
Bernhard Pottler <bpottler@nexgo.de>
2024-12-27 20:00:33 +0100

    feat: add locale for 'zh-CN'

---
+++ src/locales/zh-cn.ts
@@ -0,0 +1,13 @@
+/**
+ * Chinese (China) [zh-CN]
+ */
+
+import localeZh from '~/locales/en'
+import { cloneLocale, setLocaleProperty } from '~/plugins/locale'
+
+const localeZhCn = cloneLocale(localeZh)
+
+// Use 'setLocaleProperty' as all properties are 'readonly'
+setLocaleProperty(localeZhCn, 'name', 'zh-CN')
+
+export default localeZhCn
