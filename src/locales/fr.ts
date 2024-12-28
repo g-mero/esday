a0a63a300a39247aca5ad6cdaa572e9ea285a52d
Bernhard Pottler <bpottler@nexgo.de>
2024-12-28 07:24:47 +0100

    feat: add locale for 'fr'

---
+++ src/locales/fr.ts
@@ -0,0 +1,53 @@
+/**
+ * French [fr]
+ */
+
+import type { Locale } from '~/plugins/locale'
+
+const localeFr: Readonly<Locale> = {
+  name: 'fr',
+  weekdays: ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'],
+  weekdaysShort: ['dim.', 'lun.', 'mar.', 'mer.', 'jeu.', 'ven.', 'sam.'],
+  weekdaysMin: ['di', 'lu', 'ma', 'me', 'je', 've', 'sa'],
+  months: ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'],
+  monthsShort: ['janv.', 'févr.', 'mars', 'avr.', 'mai', 'juin', 'juil.', 'août', 'sept.', 'oct.', 'nov.', 'déc.'],
+  weekStart: 1,
+  yearStart: 4,
+  formats: {
+    LT: 'HH:mm',
+    LTS: 'HH:mm:ss',
+    L: 'DD/MM/YYYY',
+    LL: 'D MMMM YYYY',
+    LLL: 'D MMMM YYYY HH:mm',
+    LLLL: 'dddd D MMMM YYYY HH:mm',
+    l: 'DD/MM/YYYY',
+    ll: 'D MMMM YYYY',
+    lll: 'D MMMM YYYY HH:mm',
+    llll: 'dddd D MMMM YYYY HH:mm',
+  },
+  relativeTime: {
+    future: 'dans %s',
+    past: 'il y a %s',
+    s: 'quelques secondes',
+    m: 'une minute',
+    mm: '%d minutes',
+    h: 'une heure',
+    hh: '%d heures',
+    d: 'un jour',
+    dd: '%d jours',
+    M: 'un mois',
+    MM: '%d mois',
+    y: 'un an',
+    yy: '%d ans',
+  },
+  meridiem: (hour: number, minute: number, isLowercase: boolean) => {
+    const m = (hour < 12 ? 'AM' : 'PM')
+    return isLowercase ? m.toLowerCase() : m
+  },
+  ordinal: (n) => {
+    const o = n === 1 ? 'er' : ''
+    return `${n}${o}`
+  },
+}
+
+export default localeFr
