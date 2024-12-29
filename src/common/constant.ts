e59bf7add2162500660ed3487ec21d783e82dad4
Bernhard Pottler <bpottler@nexgo.de>
2024-12-29 10:15:45 +0100

    refactor: export all constants from constant.ts as default

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -30,3 +30,29 @@ export const INVALID_DATE_STRING = 'Invalid Date'
 // eslint-disable-next-line regexp/no-misleading-capturing-group
 export const REGEX_PARSE = /^(\d{4})[-/]?(\d{1,2})?[-/]?(\d{0,2})[T\s]*(\d{1,2})?:?(\d{1,2})?:?(\d{1,2})?[.:]?(\d+)?$/i
 export const REGEX_FORMAT = /\[([^\]]+)\]|Y{1,4}|M{1,4}|D{1,2}|d{1,4}|H{1,2}|h{1,2}|a|A|m{1,2}|s{1,2}|Z{1,2}|SSS/g
+
+export default {
+  SECONDS_A_MINUTE,
+  SECONDS_A_HOUR,
+  SECONDS_A_DAY,
+  SECONDS_A_WEEK,
+  MILLISECONDS_A_SECOND,
+  MILLISECONDS_A_MINUTE,
+  MILLISECONDS_A_HOUR,
+  MILLISECONDS_A_DAY,
+  MILLISECONDS_A_WEEK,
+  INDEX_MONDAY,
+  MS,
+  SECOND,
+  MIN,
+  HOUR,
+  DAY,
+  WEEK,
+  MONTH,
+  YEAR,
+  DATE,
+  FORMAT_DEFAULT,
+  INVALID_DATE_STRING,
+  REGEX_PARSE,
+  REGEX_FORMAT,
+}
