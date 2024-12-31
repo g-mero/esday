6810a10639abb4f6a6980dbad099ab2ad088820b
g-mero <gmero@foxmail.com>
2024-12-31 17:26:38 +0800

    chore: use moment instead of dayjs for testing

--- test/plugins/utc.test.ts
+++ test/plugins/utc.test.ts
@@ -1,22 +1,20 @@
-import dayjs from 'dayjs'
-import dayjsUTC from 'dayjs/plugin/utc'
 import { esday } from 'esday'
+import moment from 'moment'
 import { expect, it } from 'vitest'
 import { utcPlugin } from '~/plugins/utc'
 
-dayjs.extend(dayjsUTC)
 esday.extend(utcPlugin)
 
 it('parse normal string', () => {
   const parseStr = '2024-12-25 12:34:56'
 
-  expect(esday.utc(parseStr).format()).toBe(dayjs.utc(parseStr).format())
-  expect(esday(parseStr).utc().format()).toBe(dayjs(parseStr).utc().format())
+  expect(esday.utc(parseStr).format()).toBe(moment.utc(parseStr).format())
+  expect(esday(parseStr).utc().format()).toBe(moment(parseStr).utc().format())
 })
 
 it('get utc field value', () => {
   const parseStr = '2024-12-25 12:34:56';
   (['year', 'month', 'date', 'hour', 'minute', 'second', 'ms'] as const).forEach((field) => {
-    expect(esday.utc(parseStr).get(field)).toBe(dayjs.utc(parseStr).get(field))
+    expect(esday.utc(parseStr).get(field)).toBe(moment.utc(parseStr).get(field))
   })
 })
