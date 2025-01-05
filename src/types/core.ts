af6d18b210e3d768b08b0c709bb636d1e60e67d4
g-mero <gmero@foxmail.com>
2025-01-06 00:39:42 +0800

    refactor: type optimized

--- src/types/core.ts
+++ src/types/core.ts
@@ -6,5 +6,5 @@ export interface EsDayFactory {
   (...args: Parameters<EsDayFactoryParserFn>): ReturnType<EsDayFactoryParserFn>
   extend: <T extends {}>(plugin: EsDayPlugin<T>, option?: T) => EsDayFactory
 }
-export type EsDayPlugin<T extends {}> = (option: T, dayTsClass: typeof EsDay, esday: EsDayFactory) => void
+export type EsDayPlugin<T extends {} = {}> = (option: T, esdayClass: typeof EsDay, esday: EsDayFactory) => void
 export type DateType = EsDay | Date | string | number | number[] | null | undefined | object
