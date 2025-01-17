87257e643af09d005bd4b82498edb09773cafcab
Bernhard Pottler <bpottler@nexgo.de>
2025-01-17 07:20:17 +0100

    feat: add plugin AdvancedParse for single format strings
    Format as array of strings and strict mode not yet implemented

---
+++ src/plugins/advancedParse/index.ts
@@ -0,0 +1,268 @@
+/* eslint-disable dot-notation */
+import type { DateType, EsDay, EsDayPlugin } from 'esday'
+import { isString, isUndefined } from '~/common'
+
+const formattingTokens
+  = /(\[[^[]*\])|([-_:/.,()\s]+)|(YYYY|YY?|MM?|DD?|hh?|HH?|mm?|ss?|S{1,3}|ZZ?|[QXx])/g
+
+// Regular expressions for parsing
+const match1 = /\d/
+const match2 = /\d{2}/
+const match3 = /\d{3}/
+const match4 = /\d{4}/
+const match1to2 = /\d\d?/
+const match1to4 = /\d{1,4}/
+const matchSigned = /[+-]?\d+/
+const matchUnsigned = /\d+/
+const matchTimestamp = /[+-]?\d+(\.\d{1,3})?/ // 123456789 123456789.123
+const matchOffset = /[+-]\d\d:?(\d\d)?|Z/
+
+interface Time {
+  year?: number
+  month?: number
+  day?: number
+  hours?: number
+  minutes?: number
+  seconds?: number
+  milliseconds?: number
+  zoneOffset?: number
+  unix?: number
+}
+
+// Helper functions
+function parseTwoDigitYear(input: string): number {
+  const inputLimited = input.slice(0, 2)
+  const year = +inputLimited
+  return year + (year > 68 ? 1900 : 2000)
+}
+
+function parseFourDigitYear(input: string): number {
+  if (input.length === 2) {
+    return parseTwoDigitYear(input)
+  }
+  else {
+    return +input
+  }
+}
+
+function offsetFromString(offset: string): number {
+  if (!offset)
+    return 0
+  if (offset === 'Z')
+    return 0
+  const parts = offset.match(/([+-]|\d\d)/g)
+  if (!parts)
+    return 0
+  const minutes = +parts[1] * 60 + (+parts[2] || 0)
+  return parts[0] === '+' ? -minutes : minutes
+}
+
+/**
+ * Create a function that will add a value from the input string
+ * to the given object ('this') containing the date&time components.
+ * @param property - name of the date&time component to add
+ * @returns function that will add the given value to date&time component
+ */
+function addInput(property: keyof Time) {
+  return function (this: Time, input: string | number) {
+    this[property] = +input
+  }
+}
+
+/**
+ * Create a function that will add a milliseconds value from the input
+ * string to the given object ('this') containing the date&time components.
+ * If the input string has more than 3 characters, only the first 3 characters
+ * get evaluated.
+ * @returns function that will add the given value to the milliseconds property of date&time component
+ */
+function addMillisecondsToInput() {
+  return function (this: Time, input: string) {
+    const inputLimited = input.slice(0, 3)
+    let value = +inputLimited
+    switch (inputLimited.length) {
+      case 1:
+        value *= 100
+        break
+      case 2:
+        value *= 10
+        break
+    }
+    this.milliseconds = value
+  }
+}
+
+/**
+ * Create a function that will add a unix timestamp value from the input
+ * string to the given object ('this') containing the date&time components.
+ * If the input string contains a dot character, the value is interpreted as
+ * seconds and converted to milliseconds.
+ * @returns function that will add the given value to the milliseconds property of date&time component
+ */
+function addUnixToInput(isMilliseconds: boolean) {
+  return function (this: Time, input: string) {
+    let value = +input
+    if (isMilliseconds) {
+      value = Number.parseInt(input, 10)
+    }
+    else {
+      value = Number.parseFloat(input) * 1000
+    }
+    this.unix = value
+  }
+}
+
+/**
+ * List of parsing tokens; the key of an entry in this list is
+ * the token to be parsed and the value is a 3-entries-array with
+ * the 1st entry being the regex for parsing in 'standard' mode,
+ * the 2nd entry being the regex for parsing in 'strict' mode and
+ * the 3rd entry being a function that will add the corresponding
+ * value to an object containing all the date&time elements (year,
+ * month, day, ...).
+ */
+// TODO signature of function must include 'strict' to be used in conversion function (e.g. in MS)
+const expressions: Record<string, [RegExp, RegExp, (this: Time, input: string) => void]> = {
+  Q: [match1, match1, function (input) {
+    this.month = (Number.parseInt(input, 10) - 1) * 3 + 1
+  }],
+  S: [matchUnsigned, match1, addMillisecondsToInput()],
+  SS: [matchUnsigned, match2, addMillisecondsToInput()],
+  SSS: [matchUnsigned, match3, addMillisecondsToInput()],
+  s: [match1to2, match1, addInput('seconds')],
+  ss: [match1to2, match2, addInput('seconds')],
+  m: [match1to2, match1to2, addInput('minutes')],
+  mm: [match1to2, match2, addInput('minutes')],
+  H: [match1to2, match1to2, addInput('hours')],
+  h: [match1to2, match1to2, addInput('hours')],
+  HH: [match1to2, match2, addInput('hours')],
+  hh: [match1to2, match2, addInput('hours')],
+  D: [match1to2, match1to2, addInput('day')],
+  DD: [match1to2, match2, addInput('day')],
+  x: [matchSigned, matchSigned, addUnixToInput(true)],
+  X: [matchTimestamp, matchTimestamp, addUnixToInput(false)],
+  M: [match1to2, match1to2, addInput('month')],
+  MM: [match1to2, match2, addInput('month')],
+  Y: [match1to4, match1to2, function (input) {
+    this.year = parseTwoDigitYear(input)
+  }],
+  YY: [match1to4, match2, function (input) {
+    this.year = parseTwoDigitYear(input)
+  }],
+  YYYY: [match1to4, match4, function (input) {
+    this.year = parseFourDigitYear(input)
+  }],
+  Z: [matchOffset, matchOffset, function (input) {
+    this.zoneOffset = offsetFromString(input)
+  }],
+  ZZ: [matchOffset, matchOffset, function (input) {
+    this.zoneOffset = offsetFromString(input)
+  }],
+}
+
+/**
+ * Build a parser that will parse an input string an return an
+ * object with the components of a date&time.
+ * @param format - format to parse
+ * @returns function that will parse an input string to a 'time' object
+ */
+function makeParser(format: string): (input: string) => Time {
+  const array: any[] = format.match(formattingTokens) || []
+  const { length } = array
+  for (let i = 0; i < length; i += 1) {
+    const token = array[i]
+    const parseTo = expressions[token]
+    const regex = parseTo && parseTo[0]
+    // TODO this is for non-strict-mode only
+    const parser = parseTo && parseTo[2]
+    if (parser as any) {
+      array[i] = { regex, parser }
+    }
+    else {
+      array[i] = token.replace(/^\[|\]$/g, '')
+    }
+  }
+  return function (input: string): Time {
+    const time: Time = {}
+    for (let i = 0, start = 0; i < length; i += 1) {
+      const token = array[i]
+      // if (typeof token === 'string') {
+      if (isString(token)) {
+        start += token.length
+      }
+      else {
+        const { regex, parser } = token
+        const part = input.slice(start)
+        const match = regex.exec(part)
+        if (match !== null) {
+          const value = match[0]
+          input = input.replace(value, '')
+          parser.call(time, value)
+        }
+        else {
+          parser.call(time, Number.NaN)
+        }
+      }
+    }
+    return time
+  }
+}
+
+/**
+ * Parse the given input using the given format and create a Date
+ * object from the parsed elements.
+ * @param input - string to be parsed
+ * @param format - format to use
+ * @param utc - will this become an utc date&time?
+ * @returns Date object generated from the given data
+ */
+function parseFormattedInput(input: string, format: string, utc: boolean): Date {
+  try {
+    const parser = makeParser(format)
+    const { year, month, day, hours, minutes, seconds, milliseconds, zoneOffset, unix } = parser(input)
+
+    if (!isUndefined(unix)) {
+      return new Date(unix)
+    }
+
+    if (utc) {
+      return new Date(Date.UTC(year || 0, (month || 1) - 1, day || 1, hours || 0, minutes || 0, seconds || 0, milliseconds || 0))
+    }
+
+    if (zoneOffset !== undefined) {
+      const offsetMs = (zoneOffset || 0) * 60000
+      return new Date(Date.UTC(year || 0, (month || 1) - 1, day || 1, hours || 0, minutes || 0, seconds || 0, milliseconds || 0) - offsetMs)
+    }
+
+    return new Date(year || 0, (month || 1) - 1, day || 1, hours || 0, minutes || 0, seconds || 0, milliseconds || 0)
+  }
+  catch {
+    return new Date(Number.NaN)
+  }
+}
+
+const advancedParsePlugin: EsDayPlugin<{}> = (_, dayTsClass: typeof EsDay) => {
+  const oldParse = dayTsClass.prototype['parse']
+  dayTsClass.prototype['parse'] = function (d?: Exclude<DateType, EsDay>) {
+    // TODO use more than 1 additional parameter e..g. 'esday(date, format, strict)
+    const format = this['$conf'].args_1
+
+    // TODO format could be an array
+    if (isString(d) && isString(format)) {
+      // utc plugin compatibility
+      const date = parseFormattedInput(d, format, !!this['$conf'].utc)
+
+      if (Number.isNaN(date.getTime())) {
+        this['$d'] = new Date('')
+      }
+      else {
+        this['$d'] = date
+      }
+    }
+    else {
+      oldParse.call(this, d)
+    }
+  }
+}
+
+export default advancedParsePlugin
---
+++ test/plugins/advancedParse.test.ts
@@ -0,0 +1,218 @@
+import { esday } from 'esday'
+import moment from 'moment'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+
+import advancedParsePlugin from '~/plugins/advancedParse'
+import { expectSame, expectSameResult } from '../util'
+
+esday.extend(advancedParsePlugin)
+
+describe('advancedParse plugin - default mode', () => {
+  describe('with format as single string', () => {
+    const fakeTimeAsString = '2023-12-17T03:24:46.234'
+
+    beforeEach(() => {
+      vi.useFakeTimers()
+      vi.setSystemTime(new Date(fakeTimeAsString))
+    })
+
+    afterEach(() => {
+      vi.useRealTimers()
+    })
+
+    it.each([
+      { formatString: 'YY', sourceString: '0', expectedString: '2000-01-01T00:00:00' },
+      { formatString: 'YY', sourceString: '3', expectedString: '2003-01-01T00:00:00' },
+      { formatString: 'YY', sourceString: '-4', expectedString: '2004-01-01T00:00:00' },
+      { formatString: 'YY', sourceString: '00', expectedString: '2000-01-01T00:00:00' },
+      { formatString: 'YY', sourceString: '68', expectedString: '2068-01-01T00:00:00' },
+      { formatString: 'YY', sourceString: '69', expectedString: '1969-01-01T00:00:00' },
+      { formatString: 'YY', sourceString: '87', expectedString: '1987-01-01T00:00:00' },
+      { formatString: 'YY', sourceString: '-53', expectedString: '2053-01-01T00:00:00' },
+      { formatString: 'YY', sourceString: '1827', expectedString: '2018-01-01T00:00:00' },
+      { formatString: 'YY', sourceString: '1987', expectedString: '2019-01-01T00:00:00' },
+      { formatString: 'YY', sourceString: '2068', expectedString: '2020-01-01T00:00:00' },
+      { formatString: 'YYYY', sourceString: '68', expectedString: '2068-01-01T00:00:00' },
+      { formatString: 'YYYY', sourceString: '69', expectedString: '1969-01-01T00:00:00' },
+      { formatString: 'YYYY', sourceString: '87', expectedString: '1987-01-01T00:00:00' },
+      { formatString: 'YYYY', sourceString: '-53', expectedString: '2053-01-01T00:00:00' },
+      { formatString: 'YYYY', sourceString: '1870', expectedString: '1870-01-01T00:00:00' },
+      { formatString: 'YYYY', sourceString: '1923', expectedString: '1923-01-01T00:00:00' },
+      { formatString: 'YYYY', sourceString: '2000', expectedString: '2000-01-01T00:00:00' },
+      { formatString: 'YYYY', sourceString: '2023', expectedString: '2023-01-01T00:00:00' },
+    ])('parse year "$sourceString" with "$formatString"', ({ sourceString, formatString, expectedString }) => {
+      expectSameResult(esday => esday(sourceString, formatString))
+      // Remove offset from formatted string to make test runnable in every timezone
+      expect(esday(sourceString, formatString).format().slice(0, -6)).toBe(expectedString)
+    })
+
+    it.each([
+      { formatString: 'Y', sourceString: '0', expectedString: '2000-01-01T00:00:00' },
+      { formatString: 'Y', sourceString: '3', expectedString: '2003-01-01T00:00:00' },
+      { formatString: 'Y', sourceString: '-4', expectedString: '2004-01-01T00:00:00' },
+      { formatString: 'Y', sourceString: '68', expectedString: '2068-01-01T00:00:00' },
+      { formatString: 'Y', sourceString: '69', expectedString: '1969-01-01T00:00:00' },
+      { formatString: 'Y', sourceString: '87', expectedString: '1987-01-01T00:00:00' },
+      { formatString: 'Y', sourceString: '-53', expectedString: '2053-01-01T00:00:00' },
+      { formatString: 'Y', sourceString: '1870', expectedString: '2018-01-01T00:00:00' },
+      { formatString: 'Y', sourceString: '1923', expectedString: '2019-01-01T00:00:00' },
+      { formatString: 'Y', sourceString: '2000', expectedString: '2020-01-01T00:00:00' },
+      { formatString: 'Y', sourceString: '2023', expectedString: '2020-01-01T00:00:00' },
+      { formatString: 'YYYY', sourceString: '0', expectedString: '1900-01-01T00:00:00' },
+      { formatString: 'YYYY', sourceString: '3', expectedString: '1903-01-01T00:00:00' },
+      { formatString: 'YYYY', sourceString: '-4', expectedString: '1904-01-01T00:00:00' },
+    ])('parse year "$sourceString" with "$formatString" - difference to moment.js', ({ sourceString, formatString, expectedString }) => {
+      // Remove offset from formatted string to make result independent of utcOffset
+      expect(esday(sourceString, formatString).format().slice(0, -6)).toBe(expectedString)
+    })
+
+    it.each([
+      { formatString: 'S', sourceString: '0', expectedMS: 0 },
+      { formatString: 'S', sourceString: '3', expectedMS: 300 },
+      { formatString: 'S', sourceString: '14', expectedMS: 140 },
+      { formatString: 'S', sourceString: '168', expectedMS: 168 },
+      { formatString: 'S', sourceString: '2369', expectedMS: 236 },
+      { formatString: 'S', sourceString: '23678901987', expectedMS: 236 },
+      { formatString: 'SS', sourceString: '0', expectedMS: 0 },
+      { formatString: 'SS', sourceString: '3', expectedMS: 300 },
+      { formatString: 'SS', sourceString: '14', expectedMS: 140 },
+      { formatString: 'SS', sourceString: '168', expectedMS: 168 },
+      { formatString: 'SS', sourceString: '2369', expectedMS: 236 },
+      { formatString: 'SS', sourceString: '23678901987', expectedMS: 236 },
+      { formatString: 'SSS', sourceString: '0', expectedMS: 0 },
+      { formatString: 'SSS', sourceString: '3', expectedMS: 300 },
+      { formatString: 'SSS', sourceString: '14', expectedMS: 140 },
+      { formatString: 'SSS', sourceString: '168', expectedMS: 168 },
+      { formatString: 'SSS', sourceString: '2369', expectedMS: 236 },
+      { formatString: 'SSS', sourceString: '23678901987', expectedMS: 236 },
+    ])('parse milliseconds "$sourceString" with "$formatString" - difference to moment.js', ({ sourceString, formatString, expectedMS }) => {
+      expect(esday(sourceString, formatString).millisecond()).toBe(expectedMS)
+      expectSame(esday => esday(sourceString, formatString).millisecond())
+    })
+
+    it.each([
+      { formatString: 'MM-YYYY-DD HH:mm:ss.SSS', sourceString: '08-2023-14 21:43:12.123', expectedString: '2023-08-14T21:43:12' },
+      { formatString: 'MM-YYYY-DD HH:mm:ss.SSS', sourceString: '8-2023-4 1:3:2.3', expectedString: '2023-08-04T01:03:02' },
+      { formatString: 'M-YY-D H:m:s.SS', sourceString: '08-2023-14 21:43:12.123', expectedString: '2020-08-14T21:43:12' },
+      { formatString: 'M-YY-D H:m:s.SS', sourceString: '8-23-4 1:3:2.3', expectedString: '2023-08-04T01:03:02' },
+      { formatString: 'Q YYYY', sourceString: '1 2023', expectedString: '2023-01-01T00:00:00' },
+      { formatString: 'Q YYYY', sourceString: '2 2023', expectedString: '2023-04-01T00:00:00' },
+      { formatString: 'Q YYYY', sourceString: '3 2023', expectedString: '2023-07-01T00:00:00' },
+      { formatString: 'Q YYYY', sourceString: '4 2023', expectedString: '2023-10-01T00:00:00' },
+      { formatString: 'x', sourceString: '1442086062579', expectedString: '2015-09-12T21:27:42' },
+      { formatString: 'X', sourceString: '1442086062.579', expectedString: '2015-09-12T21:27:42' },
+      { formatString: 'X', sourceString: '1442086062', expectedString: '2015-09-12T21:27:42' },
+    ])('parse date&time "$sourceString" with "$formatString"', ({ sourceString, formatString, expectedString }) => {
+      expectSameResult(esday => esday(sourceString, formatString))
+      // Remove offset from formatted string to make test runnable in every timezone
+      expect(esday(sourceString, formatString).format().slice(0, -6)).toBe(expectedString)
+    })
+
+    it.each([
+      '-',
+      '_',
+      ',',
+      '(',
+      ')',
+      '/',
+    ])('using "%s" as separator', (sep) => {
+      const sourceString = '17?12?2023?03?24?46?234'
+      const formatString = `DD${sep}MM${sep}YYYY${sep}HH${sep}mm${sep}ss${sep}SSS`
+
+      expectSameResult(esday => esday(sourceString, formatString))
+    })
+
+    it('parse with "." as separator', () => {
+      const sourceString = '17.12.2023T03.12.46.234'
+      const formatString = `DD.MM.YYYYTHH.mm.ss.SSS`
+
+      expectSameResult(esday => esday(sourceString, formatString))
+    })
+
+    it('parse with ":" as separator', () => {
+      const sourceString = '17:12:2023T03:12:46.234'
+      const formatString = `DD:MM:YYYYTHH:mm:ss:SSS`
+
+      expectSameResult(esday => esday(sourceString, formatString))
+    })
+
+    it('parse with different separator in source and format', () => {
+      const sourceString = '2024/07/09T21:27:34.000'
+      const formatString = 'YYYY-MM-DD HH-mm-ss-SSS'
+
+      expectSameResult(esday => esday(sourceString, formatString))
+    })
+
+    it('parse with text as separator', () => {
+      const inst = esday('2024年07月月09日日.00时', 'YYYY年MM月月DD日日.HH时')
+
+      expect(inst.format('YYYY-MM-DD HH:mm:ss')).toBe('2024-07-09 00:00:00')
+    })
+
+    it('parse invalid date with overflow', () => {
+      const sourceString = '35/22/2010 99:88:77'
+      const formatString = 'DD-MM-YYYY HH:mm:ss'
+      const expectedString = '2011-11-08T04:29:17'
+      const esdayParsed = esday(sourceString, formatString)
+      const momentParsed = moment(sourceString, formatString)
+
+      expect(esdayParsed.isValid()).toBeTruthy()
+      expect(momentParsed.isValid()).toBeFalsy()
+      // Remove offset from formatted string, as esday uses local timezone for offset
+      expect(esday(sourceString, formatString).format().slice(0, -6)).toBe(expectedString)
+    })
+
+    it('parse invalid quarter with overflow', () => {
+      const sourceString = '5 2023'
+      const formatString = 'Q YYYY'
+      const expectedString = '2024-01-01T00:00:00'
+      const esdayParsed = esday(sourceString, formatString)
+      const momentParsed = moment(sourceString, formatString)
+
+      expect(esdayParsed.isValid()).toBeTruthy()
+      expect(momentParsed.isValid()).toBeFalsy()
+      // Remove offset from formatted string, as esday uses local timezone for offset
+      expect(esday(sourceString, formatString).format().slice(0, -6)).toBe(expectedString)
+    })
+
+    it('parse with "0" as day or month (using default values)', () => {
+      const sourceString = '1970-00-00'
+      const formatString = 'YYYY-MM-DD'
+      const expectedString = '1970-01-01T00:00:00'
+      const esdayParsed = esday(sourceString, formatString)
+      const momentParsed = moment(sourceString, formatString)
+
+      expect(esdayParsed.isValid()).toBeTruthy()
+      expect(momentParsed.isValid()).toBeFalsy()
+      // Remove offset from formatted string, as esday uses local timezone for offset
+      expect(esday(sourceString, formatString).format().slice(0, -6)).toBe(expectedString)
+    })
+
+    it('parse date not matching format', () => {
+      const sourceString = '10/12/2014'
+      const formatString = 'YYYY-MM-DD'
+      const expectedString = '2010-12-20T00:00:00'
+
+      expectSameResult(esday => esday(sourceString, formatString))
+      // Remove offset from formatted string to make test runnable in every timezone
+      expect(esday(sourceString, formatString).format().slice(0, -6)).toBe(expectedString)
+    })
+  })
+
+  describe('parse with format as array of strings', () => {
+    // TODO activate after adding array formats to parse
+    // it('first match vs. longest match', () => {
+    //   const sourceString = '2012-05-28 10:21:15'
+    //   const formatString =  ['YYYY', 'YYYY-MM-DD', 'YYYY-MM-DD HH:mm:ss']
+    //   const expectedString = '2012-01-01 00:00:00' // moment: '2012-05-28 10:21:15'
+
+    //   expectSameResult(esday => esday(sourceString, formatString))
+    //   // Remove offset from formatted string to make test runnable in every timezone
+    //   expect(esday(sourceString, formatString).format().slice(0, -6)).toBe(expectedString)
+    // })
+  })
+})
+
+it.todo('with strict mode')
+
+// TODO add tests for utc in separate file
