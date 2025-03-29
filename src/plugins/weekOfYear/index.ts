98d812204894ba293debe99b8a277703c7c5edc7
Bernhard Pottler <bpottler@nexgo.de>
2025-03-29 10:19:02 +0100

    fix: weekOfYear fails for 1st day of week after start of DST

--- src/plugins/weekOfYear/index.ts
+++ src/plugins/weekOfYear/index.ts
@@ -1,6 +1,5 @@
 import type { EsDayPlugin } from 'esday'
 import { C } from '~/common'
-import { INDEX_THURSDAY, MILLISECONDS_A_WEEK } from '~/common/constants'
 
 declare module 'esday' {
   interface EsDay {
@@ -12,11 +11,13 @@ declare module 'esday' {
 const weekOfYearPlugin: EsDayPlugin<{}> = (_, dayClass) => {
   // @ts-expect-error function is compatible with its overload
   dayClass.prototype.week = function (week?: number) {
+    // Setter
     if (week) {
       return this.add((week - this.week()) * 7, C.DAY)
     }
 
-    const yearStart = this.localeObject?.().yearStart || INDEX_THURSDAY // default to Thursday according to ISO 8601
+    // Getter
+    const yearStart = this.localeObject?.().yearStart || C.INDEX_THURSDAY // default to Thursday according to ISO 8601
     if (this.month() === 11 && this.date() > 25) {
       const nextYearStartDay = this.startOf(C.YEAR).add(1, C.YEAR).date(yearStart)
       const thisEndOfWeek = this.endOf(C.WEEK)
@@ -26,7 +27,9 @@ const weekOfYearPlugin: EsDayPlugin<{}> = (_, dayClass) => {
     }
     const yearStartDay = this.startOf(C.YEAR).date(yearStart)
     const yearStartWeek = yearStartDay.startOf(C.WEEK).subtract(1, C.MS)
-    const diffInWeek = (this.valueOf() - yearStartWeek.valueOf()) / MILLISECONDS_A_WEEK
+    // TODO to avoid problems with (local) DST rules,
+    // we must calculate the diff in days or use utc (?)
+    const diffInWeek = (this.valueOf() - yearStartWeek.valueOf()) / C.MILLISECONDS_A_WEEK
     if (diffInWeek < 0) {
       return this.startOf(C.WEEK).week()
     }
