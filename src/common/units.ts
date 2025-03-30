119c491eb7ceddc4959fba8fe865fda931fcf0be
Bernhard P. <6582465+BePo65@users.noreply.github.com>
2025-03-30 14:07:36 +0200

    feat: add method 'diff' to EsDay (#59)
    * feat: add unit 'Quarter'
    
    * feat: add method 'diff' to EsDay
    
    * fix: fix warning from linting in CI pipeline
    
    * docs: add comments to monthDiff function
    
    * docs: add documentation for diff method

--- src/common/units.ts
+++ src/common/units.ts
@@ -1,15 +1,16 @@
-import { DATE, DAY, HOUR, MIN, MONTH, MS, SECOND, WEEK, YEAR } from './constants'
+import { DATE, DAY, HOUR, MIN, MONTH, MS, QUARTER, SECOND, WEEK, YEAR } from './constants'
 
 const UNIT_MAP = {
   y: YEAR,
+  Q: QUARTER,
   M: MONTH,
+  w: WEEK,
   D: DATE,
   d: DAY,
   h: HOUR,
   m: MIN,
   s: SECOND,
   ms: MS,
-  w: WEEK,
 } as const
 
 export type ShortUnit = keyof typeof UNIT_MAP
@@ -18,14 +19,15 @@ export type UnitType = ShortUnit | PrettyUnitType
 export type PrettyUnit<T extends UnitType> = T extends ShortUnit ? (typeof UNIT_MAP)[T] : T
 type UnionUnit<T extends ShortUnit> = T | PrettyUnit<T>
 export type UnitYear = UnionUnit<'y'>
+export type UnitQuarter = UnionUnit<'Q'>
 export type UnitMonth = UnionUnit<'M'>
+export type UnitWeek = UnionUnit<'w'>
 export type UnitDate = UnionUnit<'D'>
 export type UnitDay = UnionUnit<'d'>
 export type UnitHour = UnionUnit<'h'>
 export type UnitMin = UnionUnit<'m'>
 export type UnitSecond = UnionUnit<'s'>
 export type UnitMs = UnionUnit<'ms'>
-export type UnitWeek = UnionUnit<'w'>
 
 export function prettyUnit<T extends UnitType>(u: T): PrettyUnit<T> {
   const maybePrettyUnit = UNIT_MAP[u as ShortUnit]
