02ffc109a73d63371aaef2ae4ac5ef3b86a274a9
Bernhard Pottler <bpottler@nexgo.de>
2025-04-18 19:48:28 +0200

    fix: add more exports to plugins/index.ts

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,9 +1,28 @@
 import advancedFormatPlugin from './advancedFormat'
+import advancedParsePlugin from './advancedParse'
+import type {
+  ParseOptions,
+  ParsedElements,
+  PostParser,
+  TokenDefinitions,
+  UpdateParsedElement,
+} from './advancedParse/types'
 import isBetweenPlugin from './isBetween'
 import isSameOrAfterPlugin from './isSameOrAfter'
 import isSameOrBeforePlugin from './isSameOrBefore'
 import isTodayPlugin from './isToday'
 import localePlugin from './locale'
+import { getLocale } from './locale'
+import type {
+  DayNames,
+  Locale,
+  LocaleFormatKeys,
+  MonthNames,
+  MonthNamesFunction,
+  MonthNamesStandaloneFormat,
+  RelativeTimeElementFunction,
+  RelativeTimeKeys,
+} from './locale'
 import minMaxPlugin from './minMax'
 import quarterOfYearPlugin from './quarterOfYear'
 import toArrayPlugin from './toArray'
@@ -13,6 +32,8 @@ import weekPlugin from './week'
 
 export {
   advancedFormatPlugin,
+  advancedParsePlugin,
+  getLocale,
   isBetweenPlugin,
   isSameOrAfterPlugin,
   isSameOrBeforePlugin,
@@ -24,4 +45,20 @@ export {
   toObjectPlugin,
   utcPlugin,
   weekPlugin,
+}
+
+export type {
+  ParsedElements,
+  ParseOptions,
+  PostParser as PostParse,
+  UpdateParsedElement,
+  TokenDefinitions,
+  DayNames,
+  MonthNames,
+  MonthNamesStandaloneFormat,
+  MonthNamesFunction,
+  RelativeTimeElementFunction,
+  RelativeTimeKeys,
+  LocaleFormatKeys,
+  Locale,
 }
