f0bbf775b0c10581ccc0f77e5c1ddc6650a18d22
Bernhard Pottler <bpottler@nexgo.de>
2025-04-22 10:32:57 +0200

    refactor: change overload signatures for dayOfYear to "standard" style

--- src/plugins/dayOfYear/index.ts
+++ src/plugins/dayOfYear/index.ts
@@ -2,16 +2,17 @@ import type { EsDayPlugin } from 'esday'
 
 declare module 'esday' {
   interface EsDay {
-    dayOfYear: (() => number) & ((dayOfYear: number) => EsDay)
+    dayOfYear(): number
+    dayOfYear(dayOfYear: number): EsDay
   }
 }
 
 const dayOfYearPlugin: EsDayPlugin<{}> = (_, dayClass, d) => {
-  // @ts-expect-error is ok
+  // @ts-expect-error function is compatible with its overload
   dayClass.prototype.dayOfYear = function (input?: number) {
     const dayOfYear =
       Math.round((d(this).startOf('day').valueOf() - d(this).startOf('year').valueOf()) / 864e5) + 1
-    return input == null ? dayOfYear : this.add(input - dayOfYear, 'day')
+    return input === undefined ? dayOfYear : this.add(input - dayOfYear, 'day')
   }
 }
 
