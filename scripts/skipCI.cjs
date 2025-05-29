a0dea9fe5846d5b8701a49a25c45d3e90202f01a
g-mero <gmero@foxmail.com>
2025-05-29 14:03:13 +0800

    chore: skip docs

--- scripts/skipCI.cjs
+++ scripts/skipCI.cjs
@@ -1,6 +1,6 @@
 const { execSync } = require('node:child_process')
 
-const SKIP_FOLDERS = ['.changeset', '.github', '.vscode', '.husky', 'scripts/skipCI.cjs']
+const SKIP_FOLDERS = ['.changeset', '.github', '.vscode', '.husky', 'scripts/skipCI.cjs', 'docs']
 
 async function main() {
   execSync('git fetch origin main')
